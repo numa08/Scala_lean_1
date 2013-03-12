39208067b622f9b4dbfdf258954cf5de857ff888
numa08 <n511287@gmail.com>
2013-03-12 22:46:19 +0900

    テンプレートのobject

---
+++ src/main/java/jp/numa08/scala_lean/JavaObj.java
@@ -0,0 +1,8 @@
+package jp.numa08.scala_lean;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+class JavaObj {
+	public static final String Today = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+}
---
+++ src/main/scala/jp/numa08/scala_lean/ScalaObj.scala
@@ -0,0 +1,7 @@
+package jp.numa08.scala_lean
+
+import java.util.Date
+
+object ScalaObj {
+	val Today = "%tF".format(new Date)
+}
